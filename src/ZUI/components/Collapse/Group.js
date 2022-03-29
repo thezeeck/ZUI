a810d8177e2982d396ecd52cec44f6a89c84d40b
thezeeck <estigma.e@gmail.com>
2022-03-29 01:06:51 -0600

    collaps props

--- src/ZUI/components/Collapse/Group.js
+++ src/ZUI/components/Collapse/Group.js
@@ -3,13 +3,45 @@ import { GroupUI } from "./styles";
 
 export const Group = (props) => {
   let indexLoop = 0;
-  const [state, setState] = useState({});
+  let initialState = {};
+  Children.map(props.children, () => {
+    indexLoop++;
+    initialState[indexLoop] = indexLoop === props.expanded;
+  }); 
+  const [state, setState] = useState(initialState);
+  const handleClick = item => {
+    console.log(state);
+    if (props.accordion) {
+      let newState = {};
+      for (const key in state) {
+        if (item == key) newState[key] = !state[key];
+        else newState[key] = false;
+      }
+      setState({...state, ...newState});
+    } else {
+      setState({...state, ...{[item]: !state[item]}});
+    }
+  }
+  indexLoop = 0;
   const childrenWithState = Children.map(props.children, child => {
     indexLoop++;
-    return cloneElement(child, {state, setState, id: indexLoop});
-  });
+    initialState[indexLoop] = indexLoop === props.expanded;
+    return cloneElement(child, {state, handleClick, id: indexLoop, animated: props.animated, gap: props.gap});
+  }); 
+  
   
   return (
-    <GroupUI>{childrenWithState}</GroupUI>
+    <GroupUI {...props}>{childrenWithState}</GroupUI>
   )
 };
+
+Group.defaultProps = {
+  accordion: true,
+  animated: false,
+  gap: 4,
+  divider: false,
+  bordered: false,
+  shadow: true,
+  borderWeight: "md",
+  expanded: 0,
+}
--- src/ZUI/components/Collapse/Items.js
+++ src/ZUI/components/Collapse/Items.js
@@ -1,8 +1,8 @@
 import { ItemContent, ItemUI } from "./styles";
 
-export const Item = ({children, state, setState, id}) => (
+export const Item = ({children, state, handleClick, id}) => (
   <ItemUI>
-    <button onClick={() => {setState({...state, ...{[id]: !state[id]}})}}>X</button>
+    <button onClick={() => {handleClick(id)}}>X</button>
     <ItemContent className={state[id] && "open"}>
       <div>
         {children}
